bd4d6a5a8484a1a93ebeaf7b4aa6328dbdafccd5
Sikander Ali <112273879+SIKANDERKUMBHAR@users.noreply.github.com>
2024-08-02 11:02:41 +0500

    added new commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World! \n deployed using terraform 23');
+  res.send('Hello, World! \n deployed using terraform 24');
 });
 
 app.listen(port, () => {
